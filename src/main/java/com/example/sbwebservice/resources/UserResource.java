b08e061fa121a50191ad14d0983e14ff2e5b6a0a
Leonardo Ikejiri <leoikejiri@gmail.com>
2020-06-09 16:13:53 -0300

    User entity and resource

---
+++ src/main/java/com/example/sbwebservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.sbwebservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sbwebservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Leo", "leo@gmail.com", "11112222", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
