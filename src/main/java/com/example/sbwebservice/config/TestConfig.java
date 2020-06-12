765c12250c3a3b572ab9c27404af0a7e3736b785
Leonardo Ikejiri <leoikejiri@gmail.com>
2020-06-12 18:19:07 -0300

    Payment, one-to-one association

--- src/main/java/com/example/sbwebservice/config/TestConfig.java
+++ src/main/java/com/example/sbwebservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.sbwebservice.entities.Category;
 import com.example.sbwebservice.entities.Order;
 import com.example.sbwebservice.entities.OrderItem;
+import com.example.sbwebservice.entities.Payment;
 import com.example.sbwebservice.entities.Product;
 import com.example.sbwebservice.entities.User;
 import com.example.sbwebservice.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
